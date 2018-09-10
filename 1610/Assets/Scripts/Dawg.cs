96375a871112fe682dad78924ab052c7f3fa6d3e
Daniel Cook <dkc315@gmail.com>
2018-09-10 15:33:31 -0600

    Scripting Part 1

---
+++ 1610/Assets/Scripts/Dawg.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dawg : Animal {
+
+	// Use this for initialization
+	void Awake ()
+	{
+		print("I am a dog, Dawg");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
