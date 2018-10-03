1ee7a1e585e70bc62f39c89668b6bafafad89c7f
Daniel Cook <dkc315@gmail.com>
2018-10-02 18:20:36 -0600

    ACompB

---
+++ 1610/Assets/Scripts/ACompB.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ACompB : MonoBehaviour {
+
+    public NameId A, B;
+
+    void Update ()
+    {
+        if (A == B)
+        {
+            print("The Lock clicks Open");
+        }
+
+        if (A != B)
+        {
+            print("The Door is Locked");
+        }
+    }
+}
