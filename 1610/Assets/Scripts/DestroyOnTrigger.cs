ae93afe871b161cebd397400d82fcc0312c4fd1a
Daniel Cook <dkc315@gmail.com>
2018-10-15 15:37:10 -0600

    THINGS AND STUFF AND MORE THINGS!

---
+++ 1610/Assets/Scripts/DestroyOnTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnTrigger : MonoBehaviour
+{
+	public float DeathTime;
+	public float RespawnTime;
+
+	
+	IEnumerator OnTriggerEnter(Collider other)
+	{
+		yield return new WaitForSeconds(DeathTime);
+		Destroy(gameObject);
+
+	}
+}
