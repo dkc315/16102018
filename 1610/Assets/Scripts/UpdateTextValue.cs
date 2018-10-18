2da97cf15fd5193612c7344e39de3453cca7f04a
Daniel Cook <dkc315@gmail.com>
2018-10-18 09:40:26 -0600

    UITextUpdate

---
+++ 1610/Assets/Scripts/UpdateTextValue.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateTextValue : MonoBehaviour
+{
+
+	public FloatData Data;
+	private Text text;
+	private string StringText;
+	
+	void Start ()
+	{
+		text = GetComponent<Text>();
+	}
+
+	void Update ()
+	{
+		StringText = Data.Value.ToString();
+		text.text = StringText;
+	}
+}
