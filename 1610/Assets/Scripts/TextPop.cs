f1967889591e01dec9b805e0f3e5800da097ecda
Daniel Cook <dkc315@gmail.com>
2018-10-19 17:08:52 -0600

    Frustration

---
+++ 1610/Assets/Scripts/TextPop.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextPop : MonoBehaviour
+{
+	public FloatData Data;
+
+	private Text text;
+	private string function;
+	public bool Add, Subtract, Multiply, Divide;
+
+	private void Start()
+	{
+		text = GetComponent<Text>();
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		Data.Value.ToString();
+		text.text = function + Data.Value;
+		if (Add)
+		{
+			function = "+";
+		}
+
+		if (Subtract)
+		{
+			function = "-";
+		}
+
+		if (Multiply)
+		{
+			function = "x";
+		}
+
+		if (Divide)
+		{
+			function = "/";
+		}
+		Show();
+	}
+
+	IEnumerator Show()
+	{
+		
+		yield return new WaitForSeconds(3);
+		
+	}
+
+	
+}
