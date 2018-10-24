35b9e59673c6c85341303e173750b95a64f24a0d
Daniel Cook <dkc315@gmail.com>
2018-10-24 15:16:33 -0600

    Improving TextPop

---
+++ 1610/Assets/Scripts/TextInstancing.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Lifetime;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextInstancing : MonoBehaviour
+{
+    public Canvas Canvas;
+    public Text Text;
+    public FloatData Data;
+    public string Function;
+    public float Lifetime;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Text.text = Function + Data.Value;
+        Delay();
+    }
+
+    IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(Lifetime);
+        Text.text = "";
+    }
+   
+}
