f6e4f827b5101af35ee474fef1fe2833c0c8e924
Daniel Cook <dkc315@gmail.com>
2018-11-12 15:44:55 -0700

    What kind of f*ckery is this

---
+++ 1610/Assets/Scripts/SelfDestructOnCollision.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestructOnCollision : MonoBehaviour
+{
+	private void OnCollisionEnter(Collision other)
+	{
+		Destroy(gameObject);
+	}
+}
