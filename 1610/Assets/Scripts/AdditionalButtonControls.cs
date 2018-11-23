85e2f6cb0fbf1e6c16747f4e0c9a0255cc205757
Daniel Cook <dkc315@gmail.com>
2018-11-23 11:10:36 -0700

    PauseMenuProblems

---
+++ 1610/Assets/Scripts/AdditionalButtonControls.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class AdditionalButtonControls : MonoBehaviour
+{
+    public Canvas PauseMenu;
+    private Instancing playerWeapon;
+    private CharacterMover playerMover;
+    private bool paused;
+
+    private void Start()
+    {
+        playerWeapon = GetComponentInChildren<Instancing>();
+        playerMover = GetComponent<CharacterMover>();
+        PauseMenu.enabled = false;
+        paused = false;
+
+        StartCoroutine(PauseMenuStart());
+    }
+
+    private void Update()
+    {
+       
+    }
+
+    IEnumerator PauseMenuStart()
+    {
+        while (true)
+        {
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                StopCoroutine(PauseMenuStop());
+                PauseMenu.enabled = true;
+                playerWeapon.enabled = false;
+                playerMover.enabled = false;
+                paused = true;
+                print("Ran");
+                yield return new WaitForSeconds(1);
+                StartCoroutine(PauseMenuStop());
+            }
+            yield return null;
+        }
+    }
+    
+    IEnumerator PauseMenuStop()
+    {
+        while (true)
+         {
+             if (paused && Input.GetKey(KeyCode.Escape))
+                {
+                    StopCoroutine(PauseMenuStart());
+                    PauseMenu.enabled = false;
+                    playerWeapon.enabled = true;
+                    playerMover.enabled = true;
+                    paused = false;
+                    print("Ran2");
+                    yield return new WaitForSeconds(1);
+                    StartCoroutine(PauseMenuStart());
+                }
+             yield return null;
+         }
+    }
+}
