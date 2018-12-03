1b0c9e96e4ea76b8d0c2d643858039973744953d
Daniel Cook <dkc315@gmail.com>
2018-12-03 12:28:53 -0700

    SuperBulletEngage

---
+++ 1610/Assets/Scripts/DestroyOther.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOther : MonoBehaviour 
+{
+
+	public float Lifetime;
+	
+	IEnumerator Start ()
+	{
+		yield return new WaitForSeconds(Lifetime);
+		Destroy(gameObject);
+	}
+
+	private void OnCollisionEnter(Collision other)
+	{
+		Destroy(other.gameObject);
+	}
+}
